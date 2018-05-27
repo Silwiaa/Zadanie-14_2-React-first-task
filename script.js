bf7f9fc3ecb89cdc9caa182fb3b4d6319aab9a39
Sylwia Krzymowska <s.krzymowska@gmail.com>
2018-05-27 19:47:06 +0200

    Finish task

--- script.js
+++ script.js
@@ -1,16 +1,23 @@
+var movies = [
+  {
+    title: 'Harry Potter',
+    desc: 'Film o czarodzieju'
+  },
+  {
+    title: 'Król Lew',
+    desc: 'Każdy zna Króla Lwa:)'
+  }
+];
+var moviesElements = movies.map(function(movie){
+        return React.createElement('li', {},
+                    React.createElement('h2', {}, movie.title),
+                    React.createElement('p', {}, movie.desc)
+                ),
+    });
 var element = 
     React.createElement('div', {},
         React.createElement('h1', {}, 'Lista filmów'),
-        React.createElement('ul', {},
-            React.createElement('li', {},
-                React.createElement('h2', {}, 'Hary Potter'),
-                React.createElement('p', {}, 'Film o czarodzieju')
-            ),
-            React.createElement('li', {},
-                React.createElement('h2', {}, "Król Lew"),
-                React.createElement('p', {}, "Każdy zna Króla Lwa:)")
-            ),
-        )
+        React.createElement('ul', {}, moviesElements)
     );
                                
 ReactDOM.render(element, document.getElementById('app'));
